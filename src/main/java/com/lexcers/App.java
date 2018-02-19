7e5b2111c99551f196e47c0b2ea104c0ce471e40
Narendar Gangula <gangulanarendar@gmail.com>
2018-02-19 14:21:22 +0530

    first push

---
+++ src/main/java/com/lexcers/App.java
@@ -0,0 +1,26 @@
+package com.lexcers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public int testResult(int a)
+    {
+    	
+    	if(a==2)
+    	{
+    		return 4;
+    	}
+    	else
+    	{
+    		return 5;
+    	}
+    }
+}
